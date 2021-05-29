49284689b85ca07c4eb48176804677c4c21b4784
oswaldomercado26 <oswaldo.mercado7383@alumnos.udg.mx>
2021-05-29 11:31:53 -0500

    first commit migraciones y seeders

---
+++ app/Http/Controllers/ProgramaController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Programa;
+use Illuminate\Http\Request;
+
+class ProgramaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $programas = Programa::get();
+        return view('programa.programa-index', compact('programas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('programa.programa-form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Programa::create($request->all());
+
+        return redirect()->route('programa.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Programa  $programa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Programa $programa)
+    {
+        return view('programa.programa-show', compact('programa'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Programa  $programa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Programa $programa)
+    {
+        return view('programa.programa-form', compact('programa'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Programa  $programa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Programa $programa)
+    {
+        Programa::where('id', $programa->id)->update($request->except('_token', '_method'));
+
+        return redirect()->route('programa.show', $programa);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Programa  $programa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Programa $programa)
+    {
+        $programa->delete();
+        return redirect()->route('programa.index');
+    }
+}
